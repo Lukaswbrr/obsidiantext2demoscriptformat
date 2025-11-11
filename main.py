32c0c04aaa2bd4e9b05384c739c885d3ffe5aa2c
Lukaswbrr <49600807+Lukaswbrr@users.noreply.github.com>
2025-11-11 20:15:56 -0300

    test main

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-clipboard_text = "Hello, World!"
+clipboard_text = ">Hello, World!"
 
 start = input("Once ready with text on clipboard, press Enter to start...")
 
 print("yey")
+
+def _add_dialogue_format(text: str) -> str:
+    if text[0] == ">":
+        new_text = text.replace(">", "")
+        text = new_text
+
+    print(text)
+
+_add_dialogue_format(clipboard_text)
